b5603f5dd57867ec39a806f7c2d73c8d73a39783
PayCra <paula.krupa@gmail.com>
2022-02-07 11:13:20 +0100

    mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import './App.css';
 import React from "react";
-import Alarm from "./Components/Alarm";
-import PlaySound from "./Components/PlaySound";
-
-//Here's the error to work on:
-//Uncaught (in promise) DOMException: play() failed because the user
-//didn't interact with the document first.
-//https://goo.gl/xX8pDD
-//Read AND deal with it by forcing the user to interact first, for example by unmuting the alarms.
 
 class App extends React.Component {
 
@@ -16,14 +8,25 @@ class App extends React.Component {
         alarmTime1: 0,
         alarmTime2: 25,
         alarmTime3: 30,
-        alarmTime4: 55
+        alarmTime4: 55,
+        RedButtonState: "RedButtonVisible",
+        StartButtonState: "StartButtonVisible",
+        StartButtonDisabled: false,
+        alarmPlayed1: {},
+        alarmPlayed2: {},
+        alarmPlayed3: {},
+        alarmPlayed4: {}
     }
 
-    myAudio = new Audio("Sounds/tap.m4a");
+    myAudio = new Audio("Sounds/3 taps.m4a")
+
+    playMySound = () => {
+        this.myAudio.play()
+    }
 
     componentDidMount() {
         this.intervalHandle = setInterval(
-            () => this.tick(),1000
+            () => this.tick(), 1000
         )
     }
 
@@ -31,45 +34,48 @@ class App extends React.Component {
         clearInterval(this.intervalHandle)
     }
 
-    lastHourPlayed;
-
     tick = () => {
-
         this.setState({myDate: new Date()})
-        if (this.lastHourPlayed !== this.state.myDate.getHours() &&
-            (
-            this.state.myDate.getMinutes()===this.state.alarmTime1 ||
-            this.state.myDate.getMinutes()===this.state.alarmTime2 ||
-            this.state.myDate.getMinutes()===this.state.alarmTime3 ||
-            this.state.myDate.getMinutes()===this.state.alarmTime4
-            )
-            ) {
-            this.myAudio.play()
-            this.lastHourPlayed = this.state.myDate.getHours()
+        if (this.state.alarmPlayed1 !== this.state.myDate.getHours() &&
+            this.state.myDate.getMinutes() === this.state.alarmTime1
+        ) {
+            this.playMySound()
+            this.setState({alarmPlayed1: this.state.myDate.getHours()})
+        }
+        else if (this.state.alarmPlayed2 !== this.state.myDate.getHours() &&
+            this.state.myDate.getMinutes() === this.state.alarmTime2
+        ) {
+            this.playMySound()
+            this.setState({alarmPlayed2: this.state.myDate.getHours()})
+        }
+        else if (this.state.alarmPlayed3 !== this.state.myDate.getHours() &&
+            this.state.myDate.getMinutes() === this.state.alarmTime3
+        ) {
+            this.playMySound()
+            this.setState({alarmPlayed3: this.state.myDate.getHours()})
+        }
+        else if (this.state.alarmPlayed4 !== this.state.myDate.getHours() &&
+            this.state.myDate.getMinutes() === this.state.alarmTime4
+        ) {
+            this.playMySound()
+            this.setState({alarmPlayed4: this.state.myDate.getHours()})
         }
     }
 
-    displayMinute = (inputMinute) => {
-        if (inputMinute <=9) {
-            return (
-                ":0" + inputMinute
-            )
-        } else {
-            return (
-                ":" + inputMinute
-            )
-        }
+    handleStartingClick = () => {
+        this.setState({StartButtonDisabled: {}})
+        this.setState({StartButtonState: "StartButtonInvisible"})
     }
 
     render() {
         return (
             <div className="App">
-                <div>
-                    <Alarm minute={this.displayMinute(this.state.alarmTime1)}/>
-                    <Alarm minute={this.displayMinute(this.state.alarmTime2)}/>
-                    <Alarm minute={this.displayMinute(this.state.alarmTime3)}/>
-                    <Alarm minute={this.displayMinute(this.state.alarmTime4)}/>
-                    <PlaySound/>
+                <div className={this.state.RedButtonState} onClick={()=>this.setState({RedButtonState: "RedButtonInvisible"})}>
+                    <button className={this.state.StartButtonState} disabled={this.state.StartButtonDisabled} onClick={this.handleStartingClick}>click me to start the timer because Chrome hates sound
+                    </button>
+                </div>
+                <div className="AppProper">
+                    Alarm will sound at :00, :25, :30 and :55.
                 </div>
             </div>
         );
